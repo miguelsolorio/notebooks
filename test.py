53ee9d27d128c7aea4ead9e60aa5a2d4aa8b601c
Miguel Solorio <miguel.solorio@microsoft.com>
2022-04-03 20:56:41 -0700

    Update

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 # import required modules
 import pandas as pd
 import matplotlib.pyplot as plt
-# create 2D array of student details
 stdData = [['S1', 'M', 13, 123, 46],
         ['S2', 'M', 12, 134, 82],
         ['S3', 'F', 14, 114, 77],
-        ['S4', 'M', 13, 136, 73],
+        ['S4', 'M', 13, 136, 74],
         ['S5', 'F', 13, 107, 56],
         ['S6', 'F', 12, 121, 80],
         ['S7', 'M', 14, 113, 76],
@@ -16,3 +15,4 @@ stdData = [['S1', 'M', 13, 123, 46],
 df = pd.DataFrame(stdData, columns = ['ID', 'Gender','Age', 'Height(cm)','Marks'] )
 df.hist()# create histogram for the numeric data(Age, Height,Marks)
 plt.show() #displaying the plot
+
